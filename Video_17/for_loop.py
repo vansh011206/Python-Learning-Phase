be18224dc2f38047ec54f62759973461356b932f
vansh011206 <vanshajs111@gmail.com>
2025-11-15 09:07:52 +0530

    Update for_loop.py

--- Video_17/for_loop.py
+++ Video_17/for_loop.py
@@ -4,7 +4,8 @@ for k in range(0,11):
 for char in "Vanshaj":
     print(char)
     if(char=='s'):
-        print("I love this char")    
+        print("I love this char")  
 
 for i in range(0,20,2):
     print(i)
+    
