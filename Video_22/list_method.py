c66e41f9bf4a99f9d9de9958a7603b8c051b2eb4
vansh011206 <vanshajs111@gmail.com>
2025-11-25 18:57:32 +0530

    Update list_method.py

--- Video_22/list_method.py
+++ Video_22/list_method.py
@@ -8,7 +8,7 @@ hello = [1,3,22,3,4,5,6,7,6543,2,78,7,54,32]
 # copy.append(69)
 # print(copy)
 # copy.insert(3,1444)
-# print(copy)
+# print(copy) 
 newlist=[87,67,5,634]
 hello.extend(newlist)
 print(hello)
